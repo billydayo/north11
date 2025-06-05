8921790dff8af3d2b7a8c6518d6d5c8a0a69e27e
billydayo <billyfang56@gmail.com>
2025-06-05 18:14:14 +0800

    調整資料表2

--- entities/User.js
+++ entities/User.js
@@ -31,7 +31,7 @@ module.exports = new EntitySchema({
       nullable: false,
       select: false
     },
-     phonenumber: {
+    phonenumber: {
       type: 'int',
       length: 10,
       nullable: true,

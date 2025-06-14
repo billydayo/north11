5eda6f8454ad15d8c2f14fb34870d8fdb73b0245
lazurite5487 <lazurite5487@gmail.com>
2025-06-14 23:40:35 +0800

    新增喜愛餐廳及刪除

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -1,68 +1,69 @@
 const { dataSource } = require('../db/data-source');
 const Favorite = require('../entities/Favorite');
-const Store = require('../entities/Store');
+const Store = require('../entities/User');
 
 async function addFavorite(req , res) {
-    const userId = req.user.id;
-    const storeId = req.params.storeId;
+    // const userId = req.user.id;
+    const userId = req.params.userId;
     const category = req.body.category;
+    const userRole = req.user.role;
 
-    const storeRepo = dataSource.getRepository(Store);
+    const storeRepo = dataSource.getRepository(User);
     const favoriteRepo = dataSource.getRepository(Favorite);
+    if(userRole != store){
+        const store = await storeRepo.findOneBy({id: userId});
+        if(!store){
+            return res.status(404).json({
+                message: '找不到店家'
+            });
+        };
 
-    const store = await storeRepo.findOneBy({id: storeId});
-    if(!store){
-        return res.status(404).json({
-            message: '找不到店家'
+        const exists = await favoriteRepo.findOne({
+            where:{
+                // user: {id: userId},
+                store: {id: userId}
+            }
         });
-    };
+        if(exists){
+            return res.status(200).json({
+                message: '已收藏'
+            })
+        };
 
-    const exists = await favoriteRepo.findOne({
-        where:{
-            // user: {id: userId},
-            store: {id: storeId}
-        }
-    });
-    if(exists){
-        return res.status(200).json({
-            message: '已收藏'
+        const favorite = favoriteRepo.create({
+            store: {id: userId},
+            category :category
+        });
+
+        await favoriteRepo.save(favorite);
+        res.status(201).json({ 
+            message: '已加入收藏'
         })
-    };
-
-    const favorite = favoriteRepo.create({
-        store: {id: storeId},
-        category :category
-    });
-
-    await favoriteRepo.save(favorite);
-    res.status(201).json({ 
-        message: '已加入收藏'
-    })
-}
-
-async function removeFavorite (req,res) {
-    const userId = req.user.id;
-    const storeId = req.params.storeId;
-
-    const favoriteRepo = dataSource.getRepository(Favorite);
-
-    const favorite = await favoriteRepo.findOne({
-        where: {
-            // user: { id: userId },
-            store: { id: storeId }
         }
-    });
+    }
+    async function removeFavorite (req,res) {
+        // const userId = req.user.id;
+        const userId = req.params.userId;
 
-    if (!favorite){
-        return res.status(404).json({ 
-            message: '未找到收藏'
+        const favoriteRepo = dataSource.getRepository(Favorite);
+
+        const favorite = await favoriteRepo.findOne({
+            where: {
+                // user: { id: userId },
+                store: { id: userId }
+            }
         });
-    } 
 
-    await favoriteRepo.remove(favorite);
-    res.status(200).json({
-        message: '已移除收藏' 
-    });
-}
+        if (!favorite){
+            return res.status(404).json({ 
+                message: '未找到收藏'
+            });
+        } 
+
+        await favoriteRepo.remove(favorite);
+        res.status(200).json({
+            message: '已移除收藏' 
+        });
+    }
 
 module.exports = {addFavorite,removeFavorite};

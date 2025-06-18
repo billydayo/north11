c5d5534ea4478e4e0f84c68c413a041078abc367
lazurite5487 <lazurite5487@gmail.com>
2025-06-18 18:44:21 +0800

    新增喜愛餐廳及刪除

--- migrations/1750243236191-CreateFavoriteTable.js
+++ migrations/1750243236191-CreateFavoriteTable.js
@@ -1,23 +1,12 @@
-const { MigrationInterface, QueryRunner } = require('typeorm');
-
-
-class CreateFavoriteTable1750073267518 {
-    name = 'CreateFavoriteTable1750073267518';
+module.exports = class CreateFavoriteTable1750243236191 {
+    name = 'CreateFavoriteTable1750243236191';
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "store" DROP CONSTRAINT "FK_store_user"`);
-        await queryRunner.query(`
-            CREATE TABLE "favorite" (
-                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
-                "user_id" uuid,
-                "favorite_user_id" uuid,
-                CONSTRAINT "PK_495675cec4fb09666704e4f610f" PRIMARY KEY ("id")
-        )
-    `);
+        await queryRunner.query(`CREATE TABLE "favorite" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "user_id" uuid, "favorite_user_id" uuid, CONSTRAINT "PK_495675cec4fb09666704e4f610f" PRIMARY KEY ("id"))`);
         await queryRunner.query(`COMMENT ON COLUMN "store"."type" IS '店家類型，存儲為 JSON 陣列'`);
         await queryRunner.query(`COMMENT ON COLUMN "store"."location" IS '位置資訊，包含 lat、lng 和 address'`);
-        await queryRunner.query(`COMMENT ON COLUMN "store"."businessHours" IS '營業時間，格式為 {...}'`);
+        await queryRunner.query(`COMMENT ON COLUMN "store"."businessHours" IS '營業時間，格式為 {mon: "10:00-21:00", tue: "10:00-21:00", wed: null, thu: null, fri: null, sat: null, sun: "公休"}'`);
         await queryRunner.query(`COMMENT ON COLUMN "store"."status" IS '店家狀態：active, inactive, suspended'`);
         await queryRunner.query(`COMMENT ON COLUMN "store"."owner_id" IS '店家擁有者 ID，關聯到 User 表'`);
         await queryRunner.query(`ALTER TABLE "store" ADD CONSTRAINT "FK_8ce7c0371b6fca43a17f523ce44" FOREIGN KEY ("owner_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
